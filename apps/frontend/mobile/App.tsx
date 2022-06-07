33e214aea959280f3c2e3206b6d92d036f29edce
r.tamoto <r.tamoto@hitotsunagu.com>
2022-06-07 10:52:31 +0900

    add metro config, because add graphql client

--- apps/frontend/mobile/App.tsx
+++ apps/frontend/mobile/App.tsx
@@ -1,12 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+} from '@apollo/client';
+import { Users } from './src/Users';
+
+const client = new ApolloClient({
+  // link: createHttpLink({ uri: 'http://192.168.0.6:3000/graphql' }),
+  // uri: 'http://192.168.0.6:3000/graphql',
+  uri: 'http://172.31.98.229:3000/graphql',
+  cache: new InMemoryCache(),
+});
 
 export default function App() {
   return (
+      <ApolloProvider client={client}>
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Users/>
       <StatusBar style="auto" />
     </View>
+      </ApolloProvider>
   );
 }
 
