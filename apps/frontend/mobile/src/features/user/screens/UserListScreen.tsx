1f71df55626f94ac5c0d585d5f89bb9d8c4e4617
r.tamoto <r.tamoto@hitotsunagu.com>
2022-06-10 17:50:01 +0900

    change style

--- apps/frontend/mobile/src/features/user/screens/UserListScreen.tsx
+++ apps/frontend/mobile/src/features/user/screens/UserListScreen.tsx
@@ -1,9 +1,17 @@
-import { View, SafeAreaView } from 'react-native';
+import { SafeAreaView, StyleSheet } from 'react-native';
 import { UserList } from '../components';
 export const UserListScreen = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.container}>
       <UserList />
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
