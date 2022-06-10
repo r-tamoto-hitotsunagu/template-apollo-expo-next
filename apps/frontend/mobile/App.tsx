98428576ab8390c15cceba00c0c61e65f2864919
r.tamoto <r.tamoto@hitotsunagu.com>
2022-06-10 12:53:58 +0900

    change structure for provider

--- apps/frontend/mobile/App.tsx
+++ apps/frontend/mobile/App.tsx
@@ -1,56 +1,18 @@
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  HttpLink,
-  split,
-} from '@apollo/client';
-import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
-import { getMainDefinition } from '@apollo/client/utilities';
 import { StatusBar } from 'expo-status-bar';
-import { createClient } from 'graphql-ws';
 import { StyleSheet, Text, View } from 'react-native';
+// TODO: aliasから読み込む
 import { Users } from './src/Users';
-
-const httpLink = new HttpLink({
-  uri: 'http://192.168.0.6:3000/graphql',
-});
-
-const wsLink = new GraphQLWsLink(
-  createClient({
-    url: 'ws://192.168.0.6:3000/graphql',
-    connectionParams: {
-      // TODO: change auth token
-      authToken: 'testToken',
-    },
-  })
-);
-
-const splitLink = split(
-  ({ query }) => {
-    const definition = getMainDefinition(query);
-    return (
-      definition.kind === 'OperationDefinition' &&
-      definition.operation === 'subscription'
-    );
-  },
-  wsLink,
-  httpLink
-);
-
-const client = new ApolloClient({
-  link: splitLink,
-  cache: new InMemoryCache(),
-});
+// TODO: aliasから読み込む
+import { AppProvider } from './src/providers/appProvider';
 
 export default function App() {
   return (
-    <ApolloProvider client={client}>
+    <AppProvider>
       <View style={styles.container}>
         <Users />
         <StatusBar style="auto" />
       </View>
-    </ApolloProvider>
+    </AppProvider>
   );
 }
 
